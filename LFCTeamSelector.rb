ab7bb1633537a518c1082ddcb17f287a074c6343
David Halewood <davidnhalewood@hotmail.com>
2017-06-18 13:53:54 +0100

    adding ability to write to and read from file

--- LFCTeamSelector.rb
+++ LFCTeamSelector.rb
@@ -3,7 +3,9 @@ module Menu
   "Welcome to my Liverpool Team Selector, please select from the following options:
   1) Add Player
   2) Show Team
-  3) Quit "
+  3) Save Your Team
+  4) Load a Previously Created Team
+  5) Quit "
   end
   def show
     menu
@@ -29,6 +31,14 @@ class Team
   def show
     all_players
   end
+  def write_to_file(filename)
+        IO.write(filename, @all_players.map(&:to_s).join("\n"))
+  end
+  def read_from_file(filename)
+        IO.readlines(filename).each do |line|
+          puts line
+        end
+  end
 end
 
 class Players
@@ -48,13 +58,21 @@ if __FILE__ == $PROGRAM_NAME
   include Promtable
     my_team = Team.new
     puts "Please choose from the following list: "
-    until ['3'].include?(user_input = prompt(show))
+    until ['5'].include?(user_input = prompt(show))
       case user_input
       when '1' 
         my_team.add(Players.new(prompt("What is the name of the player you'd like to add?"), prompt("What position? (eg., GK/RB/LW)")))
       when '2'
         puts "Here is your team:" 
         puts my_team.show
+      when '3'
+        my_team.write_to_file(prompt("What is the filename you'd like to save to?"))
+      when '4'
+        begin
+        my_team.read_from_file(prompt("What is the filename you'd like to load?"))
+        rescue Errno::ENOENT
+          puts 'Filename not found, please verify your filename and path.'
+        end
       else 
         puts "Sorry I didn't get that."
       end
